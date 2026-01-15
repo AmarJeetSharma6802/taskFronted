439e5d7512e400d4b249afe0d2707a75e26d4b5e
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2026-01-15 16:49:46 +0530

    add privious code

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -18,21 +18,19 @@ function Navbar() {
           return;
         }
 
-        const res = await fetch(`${process.env.REACT_APP_URL}/api/user`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
+        const res = await fetch(
+          "https://backend-task-28rk.onrender.com",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-        // const data = await res.json();
+        const data = await res.json();
         // console.log("USER RESPONSE:", res.status, data);
-
-        const text = await res.text();
-      // console.log("RAW RESPONSE:", text);
-
-      const data = JSON.parse(text);
 
         if (res.ok && data.user) {
           setUser(data.user);
@@ -86,7 +84,10 @@ function Navbar() {
 
         {!loading && user && (
           <div className="avatar-wrapper" ref={menuRef}>
-            <div className="avatar" onClick={() => setOpen((prev) => !prev)}>
+            <div
+              className="avatar"
+              onClick={() => setOpen((prev) => !prev)}
+            >
               {firstChar}
             </div>
 

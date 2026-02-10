699d5f9f92c7e5f93baba2077b4f83b7ffeff245
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2026-02-10 13:11:28 +0530

    add styling for task management components and implement search functionality

--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,40 +1,42 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "../app.css";
 
 export default function Task() {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [editId, setEditId] = useState(null);
+  const [search, setSearch] = useState("");
+
 
   const token = localStorage.getItem("token");
-
   const API = import.meta.env.VITE_API_URL;
 
   const fetchTasks = async () => {
-      const res = await axios.get(`${API}/api/getTask`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      setTasks(res.data.findTask);
-    };
+    const res = await axios.get(`${API}/api/getTask`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    setTasks(res.data.findTask);
+  };
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    if (token) fetchTasks();
+  }, [token]);
 
   const handleSubmit = async () => {
     if (editId) {
       await axios.put(
         `${API}/api/updateTaks/${editId}`,
         { title, description },
-        { headers: { Authorization: `Bearer ${token}` } },
+        { headers: { Authorization: `Bearer ${token}` } }
       );
       setEditId(null);
     } else {
       await axios.post(
         `${API}/api/createTask`,
         { title, description },
-        { headers: { Authorization: `Bearer ${token}` } },
+        { headers: { Authorization: `Bearer ${token}` } }
       );
     }
 
@@ -56,37 +58,55 @@ export default function Task() {
     fetchTasks();
   };
 
+  const filteredTasks = tasks.filter((task) =>
+  task.title.toLowerCase().includes(search.toLowerCase()) ||
+  task.description.toLowerCase().includes(search.toLowerCase())
+);
+
   return (
-    <div className="" style={{minHeight:"100vh" , color:'white'}}>
-      <div className="">
+    <div className="task-container">
+
+      <input
+  className="search-input"
+  placeholder="Search task..."
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+/>
+
+      <div className="input-row">
         <input
-          className=""
+          className="task-input"
           value={title}
           placeholder="Title"
           onChange={(e) => setTitle(e.target.value)}
         />
         <input
-          className=""
+          className="task-input"
           value={description}
           placeholder="Description"
           onChange={(e) => setDescription(e.target.value)}
         />
-        <button onClick={handleSubmit} className="5">
+      </div>
+
+      <div className="btn-wrapper">
+        <button onClick={handleSubmit} className="add-btn">
           {editId ? "Update" : "Add"}
         </button>
       </div>
 
-      {tasks.map((task) => (
-        <div key={task._id} className="">
+      {filteredTasks.map((task) => (
+        <div key={task._id} className="task-card">
           <div>
             <h4>{task.title}</h4>
             <p>{task.description}</p>
           </div>
 
-          <div className="">
-            <button onClick={() => handleEdit(task)}>Edit</button>
+          <div className="task-actions">
+            <button className="edit-btn" onClick={() => handleEdit(task)}>
+              Edit
+            </button>
             <button
-              className="0"
+              className="delete-btn"
               onClick={() => handleDelete(task._id)}
             >
               Delete
@@ -97,3 +117,4 @@ export default function Task() {
     </div>
   );
 }
+
